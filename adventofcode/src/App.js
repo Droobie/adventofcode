c6a4e42be6e114e7fbb45b1f5bf97f680760c325
Drewes Bos <drewes.bos@nos.nl>
2022-01-21 10:55:55 +0100

    init basic react project and the first exercise

---
+++ adventofcode/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Exercise1 } from "./Exercises/Exercise1.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Exercise1 />
+    </div>
+  );
+}
+
+export default App;
