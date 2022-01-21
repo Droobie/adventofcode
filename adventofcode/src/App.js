2fc2302d86cda899ea1a202725b90b06d30e88bc
Drewes Bos <drewes.bos@nos.nl>
2022-01-21 17:03:32 +0100

    add both parts of exercise 2

--- adventofcode/src/App.js
+++ adventofcode/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Exercise1 } from "./Exercises/Exercise1.jsx";
+import { Exercise2 } from "./Exercises/Exercise2.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Exercise1 />
+      {/* <Exercise1 /> */}
+      <Exercise2 />
     </div>
   );
 }
