e7185737a47e40426e04dc6d4678dec3ae7e339c
Drewes Bos <drewes.bos@nos.nl>
2022-02-11 17:02:48 +0100

    add exercise 3, part 1 of exercise 4 and started with part 2

--- adventofcode/src/App.js
+++ adventofcode/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Exercise1 } from "./Exercises/Exercise1.jsx";
 import { Exercise2 } from "./Exercises/Exercise2.jsx";
+import { Exercise3 } from "./Exercises/Exercise3.jsx";
+import { Exercise4 } from "./Exercises/Exercise4.jsx";
 
 function App() {
   return (
     <div className="App">
       {/* <Exercise1 /> */}
-      <Exercise2 />
+      {/* <Exercise2 /> */}
+      {/* <Exercise3 /> */}
+      <Exercise4 />
     </div>
   );
 }
