ddceb71a5495f7eed831ddc5903357d30a97c65b
Drewes Bos <drewes.bos@nos.nl>
2023-12-08 11:59:10 +0100

    AoC 2023 day 1

--- adventofcode/src/App.js
+++ adventofcode/src/App.js
@@ -1,18 +1,10 @@
 import "./App.css";
-import { Exercise1 } from "./Exercises/Exercise1.jsx";
-import { Exercise2 } from "./Exercises/Exercise2.jsx";
-import { Exercise3 } from "./Exercises/Exercise3.jsx";
-import { Exercise4 } from "./Exercises/Exercise4.jsx";
-import { Exercise5 } from "./Exercises/Exercise5.jsx";
+import { Exercise1 } from "./Exercises/2023/Exercise1.jsx";
 
 function App() {
   return (
     <div className="App">
-      {/* <Exercise1 /> */}
-      {/* <Exercise2 /> */}
-      {/* <Exercise3 /> */}
-      {/* <Exercise4 /> */}
-      <Exercise5 />
+      <Exercise1 />
     </div>
   );
 }
