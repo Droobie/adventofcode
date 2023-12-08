8df6a87d6a61d27c6f09e2a3ed8a5b8ff8491704
Drewes Bos <drewes.bos@nos.nl>
2023-12-08 14:54:12 +0100

    exercise 3 part one solution

--- adventofcode/src/App.js
+++ adventofcode/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Exercise1 } from "./Exercises/2023/Exercise1.jsx";
 import { Exercise2 } from "./Exercises/2023/Exercise2.jsx";
+import { Exercise3 } from "./Exercises/2023/Exercise3.jsx";
 
 function App() {
   return (
     <div className="App">
       {/* <Exercise1 /> */}
-      <Exercise2 />
+      {/* <Exercise2 /> */}
+      <Exercise3 />
     </div>
   );
 }
